4f5362d3fb5eb504ca4487c9d3d0d4b88baa61f6
hieudevx <hoangtrunghieu10licbn@gmail.com>
2019-05-28 13:02:39 +0700

    locale today

--- src/CalendarControls.js
+++ src/CalendarControls.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import moment from 'moment';
 
 const propTypes = {
   year: PropTypes.number.isRequired,
@@ -22,7 +23,7 @@ const CalendarControls = props => {
   if (showTodayButton) {
     todayButton = (
       <div className="control today" onClick={() => goToToday()}>
-        Today
+        {`${moment.locale() === 'vi' ? 'Hôm nay' : Today}`}
       </div>
     );
   }
